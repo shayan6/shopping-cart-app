703c059a6179c21812e8ecb86c586958973faccd
Shayan Shaikh <shayanshaikh996@gmail.com>
2023-11-09 14:58:14 +0200

    Code fixes

--- src/components/Dashboard/ShopsTable.tsx
+++ src/components/Dashboard/ShopsTable.tsx
@@ -1,8 +1,59 @@
-import { Button, Col, Divider, Row, Space, Tag } from "antd";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import React from "react";
+import { Col, Divider, Row, Tag, Table, Avatar } from "antd";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import {
+  ShopOutlined, // Import the outlined icon you want to use
+} from "@ant-design/icons";
 
 export default function ShopsTable() {
+  const { shops } = useSelector((state: RootState) => state.shop);
+
+  const columns = [
+    {
+      title: "Avatar",
+      dataIndex: "id",
+      key: "id",
+      render: (id: string) => (
+        <span className="nowrap">
+          <Avatar style={{ backgroundColor: '#1890ff' }} icon={<ShopOutlined />} />
+        </span>
+      ),
+    },
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Sort order",
+      dataIndex: "sortOrder",
+      key: "sortOrder",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      render: (status: string) => (
+        <span className="nowrap">
+          <Tag color={status === "Open" ? "green" : "blue"}>{status}</Tag>
+        </span>
+      ),
+    },
+  ];
+
+  const data = shops?.map((shop) => ({
+    key: shop.id,
+    id: shop.id,
+    name: shop.name,
+    sortOrder: shop.sortOrder,
+    status: "Open", // Actions will be handled in the render function
+  }));
+
   return (
     <Row>
       <Col span={24}>
@@ -10,254 +61,7 @@ export default function ShopsTable() {
           Shops
         </Divider>
         <div className="table-responsive">
-          <table className="table table-padded">
-            <thead>
-              <tr>
-                <th>Assigned Doctor</th>
-                <th>Cutomer</th>
-                <th>Last Comment</th>
-                <th>Reply Date</th>
-                <th className="text-center">Status</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>John Mayers</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    I have enabled the software for you, you can try now...
-                  </div>
-                </td>
-                <td>
-                  <span>Today</span>
-                  <span className="smaller lighter">1:52am</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller green"></span>
-                  <span>
-                    <Tag color="blue">Active</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>Mike Bishop</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    Please approve this request so we can move...
-                  </div>
-                </td>
-                <td>
-                  <span>Jan 19th</span>
-                  <span className="smaller lighter">3:22pm</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller red"></span>
-                  <span>
-                    <Tag color="green">Closed</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>Terry Crews</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    We will need some login credentials to make...
-                  </div>
-                </td>
-                <td>
-                  <span>Yesterday</span>
-                  <span className="smaller lighter">7:45am</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller yellow"></span>
-                  <span>
-                    <Tag color="orange">Pending</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>Phil Collins</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    I have enabled the software for you, you can try now...
-                  </div>
-                </td>
-                <td>
-                  <span>Jan 23rd</span>
-                  <span className="smaller lighter">2:12pm</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller yellow"></span>
-                  <span>
-                    <Tag color="orange">Pending</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>Katy Pilsner</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    I have tried this solution but it does not open...
-                  </div>
-                </td>
-                <td>
-                  <span>Jan 12th</span>
-                  <span className="smaller lighter">9:51am</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller green"></span>
-                  <span>
-                    <Tag color="green">Active</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="user-with-avatar">
-                    <img
-                      alt=""
-                      src="https://avatars.githubusercontent.com/u/42208796?v=4"
-                    />
-                    <span>Wes Morgan</span>
-                  </div>
-                </td>
-                <td>
-                  <span>Shayan Shaikh</span>
-                </td>
-                <td>
-                  <div className="smaller lighter">
-                    I have enabled the software for you, you can try now...
-                  </div>
-                </td>
-                <td>
-                  <span>Jan 9th</span>
-                  <span className="smaller lighter">12:45pm</span>
-                </td>
-                <td className="nowrap">
-                  <span className="status-pill smaller yellow"></span>
-                  <span>
-                    <Tag color="orange">Pending</Tag>
-                  </span>
-                </td>
-                <td className="row-actions">
-                  <Space size={24}>
-                    <Button>
-                      <EditOutlined />
-                    </Button>
-                    <Button>
-                      <DeleteOutlined />
-                    </Button>
-                  </Space>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <Table columns={columns} dataSource={data} />
         </div>
       </Col>
     </Row>
--- src/components/Shop/ShopList.tsx
+++ src/components/Shop/ShopList.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
-import { Card } from "antd";
+import { Avatar, Card, Space, Typography } from "antd";
 import { Shop } from "../../types";
+import {
+  ShopOutlined, // Import the outlined icon you want to use
+} from "@ant-design/icons";
 
 const ShopList = () => {
   const shops: Shop[] = useSelector((state: RootState) => state.shop.shops);
@@ -13,12 +16,18 @@ const ShopList = () => {
 
   return (
     <Card title="Shops">
-      {shops
-        ?.map((shop) => (
-          <Card.Grid key={shop.id} style={gridStyle}>
-            {shop.name}
-          </Card.Grid>
-        ))}
+      {shops?.map((shop) => (
+        <Card.Grid key={shop.id} style={gridStyle}>
+          <Space direction="vertical">
+            <Avatar
+              icon={<ShopOutlined />}
+              style={{ backgroundColor: "#7bc617" }}
+            />{" "}
+            {/* Use the outlined icon here */}
+            <Typography.Text>{shop.name}</Typography.Text>
+          </Space>
+        </Card.Grid>
+      ))}
     </Card>
   );
 };
